ee6c0b4768b3f21595622658b03fdbacc13460b6
rimham <rimhammai45@gmail.com>
2025-11-14 00:14:07 +0100

    register user functionalite and the home

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -83,6 +84,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -123,8 +125,17 @@ STATIC_ROOT = BASE_DIR/ 'static/'
 STATICFILES_DIRS = [
     'foodOnline_main/static'
 ]
+#Mediaa files configuration 
+MEDIA_URL ='/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -16,10 +16,19 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+from foodOnline_main import settings
 from . import views 
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-]
+    path('accounts/' , include('accounts.urls')),
+   
+
+
+   
+] + static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
